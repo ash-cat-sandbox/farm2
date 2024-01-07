98855ea35797dd78964b17dfe12edf49d313d82f
Ash Pedicini <ash.cat.sandbox@gmail.com>
2024-01-06 16:59:01 -0800

    rectangles added

--- second_scene.py
+++ second_scene.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout, QMainWindow, QStackedWidget
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import Qt, QRect
-import sys
+import subprocess
 from rectangle_drawer import RectangleDrawer
+from crop_add import CropAdd
 
 class Ui_PageSecond(QMainWindow):
        #Page2
@@ -13,13 +14,25 @@ class Ui_PageSecond(QMainWindow):
         self.rectangle_drawer_button = QPushButton('Open Rectangle Drawer')
         layout.addWidget(self.rectangle_drawer_button)
 
+        self.crop_add_button = QPushButton('Add Crop')
+        layout.addWidget(self.crop_add_button)
+
         self.rectangle_drawer = None
+        self.crop_add = None
 
         self.setCentralWidget(self.central_widget)  # Setting central widget
 
         widget.addWidget(self)
 
         self.rectangle_drawer_button.clicked.connect(self.showRectangleDrawer)
+        self.crop_add_button.clicked.connect(self.call_crop_add)
+
+    def call_crop_add(self):
+        print("add crop button clicked")
+        if not self.crop_add:
+            self.crop_add = CropAdd()
+        self.crop_add.show()
+        
         
     def showRectangleDrawer(self):
         print("Button Clicked!")
