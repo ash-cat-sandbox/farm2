3e87769ed73e0bca94866df363dd5925e0d66556
Ash Pedicini <ash.cat.sandbox@gmail.com>
2024-01-07 06:25:24 -0800

    Added Destroy button to destroy rectangles

--- crop_add.py
+++ crop_add.py
@@ -10,6 +10,8 @@ class CropAdd(QWidget):
         self.rect_width = 50
         self.rect_height = 50
         self.cursor_pixmap = self.create_cursor_pixmap()
+        self.clear_mode = False # Flag to determine wether the widget is in clear c rops mode
+        self.create_mode = True # Flag to control crop creation
 
     def create_cursor_pixmap(self):
         # Create a pixmap with the dimensions of the rectangle
@@ -35,12 +37,32 @@ class CropAdd(QWidget):
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
+            if self.clear_mode:
+                # Remove the rectangle that contains the clicked position
+                clicked_rect = self.get_clicked_rect(event.pos())
+                if clicked_rect:
+                    self.rectangles.remove(clicked_rect)
+                    self.update() # refresh the widget to remove the rectangle
             # Create a rectangle with a specific size (e.g., 100x50) at the clicked position
-            rect_width = 50
-            rect_height = 50
-            rect = QRect(event.pos().x() - rect_width // 2, event.pos().y() - rect_height // 2, rect_width, rect_height)
-            self.rectangles.append(rect)
-            self.update()  # Refresh the widget to display the new rectangle
+            elif self.create_mode: # Only create rectangles when in create mode
+                rect_width = 50
+                rect_height = 50
+                rect = QRect(event.pos().x() - rect_width // 2, event.pos().y() - rect_height // 2, rect_width, rect_height)
+                self.rectangles.append(rect)
+                self.update()  # Refresh the widget to display the new rectangle
+
+    def get_clicked_rect(self, click_pos):
+        # Check if the click position is inside any existing rectangle
+        for rect in self.rectangles:
+            if rect.contains(click_pos):
+                return rect
+        return None
+    
+    def set_clear_mode(self, clear):
+        self.clear_mode = clear
+
+    def set_create_mode(self, create):
+        self.create_mode = create
 
     def mouseMoveEvent(self, event):
         # change cursor
